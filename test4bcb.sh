7dcd2a2a3403885ac3c29b4939bc4b0237057947
Miroslav Lichvar <mlichvar@redhat.com>
2016-04-13 14:20:27 +0200

    add tests for peer authentication

---
+++ test4bcb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+. test.common
+
+timeofflimit=2e-3
+freqofflimit=10e-6
+auth=symmSHA1
+
+test_ntppeer 100000 10000 "symmetric key, SHA1"
+
+check_sync 3 1000 10000 || test_fail
+check_sync 4 2000 11000 || test_fail
+check_stats 3 || test_fail
+check_stats 4 || test_fail
+
+test_success
